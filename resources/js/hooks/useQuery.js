cf3670277cf6e61fb92c48c52c517e6f12d2fc93
jethro.santos <jnds@newmediastaff.com>
2022-09-23 13:55:17 +0800

    delete all

--- resources/js/hooks/useQuery.js
+++
@@ -1,6 +0,0 @@
-import { useLocation } from '@reach/router';
-
-export const useQuery = (queryParam) => {
-  const search = new URLSearchParams(useLocation().search);
-  return search.get(queryParam);
-};
